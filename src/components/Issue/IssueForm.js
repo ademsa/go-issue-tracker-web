5f7d4555d487fb2e4bc8a2468c924b3a6a6613a0
Adem <63111861+ademsa@users.noreply.github.com>
2020-12-24 16:41:47 +0100

    Added validation for label, project and issue-related forms

--- src/components/Issue/IssueForm.js
+++ src/components/Issue/IssueForm.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { createFragmentContainer } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro'
 import PropTypes from 'prop-types'
+import { useForm } from 'react-hook-form';
 import { makeStyles, Grid, Button, Paper, MenuItem, InputLabel, Select, TextField, FormControl, FormHelperText, Typography } from '@material-ui/core';
 import { Save, Cancel } from '@material-ui/icons';
 import LabelsAutocomplete from './LabelsAutocomplete';
@@ -14,6 +15,12 @@ const useStyles = makeStyles((theme) => ({
         color: theme.palette.primary.main,
         textAlign: 'center',
         padding: theme.spacing(3),
+    },
+    Form: {
+        width: '100%'
+    },
+    FormControl: {
+        marginBottom: theme.spacing(1),
     },
     FormInput: {
         color: theme.palette.primary.main,
@@ -48,6 +55,14 @@ function IssueForm(props) {
         ))
     }
 
+    const { register, handleSubmit, setValue, errors } = useForm({
+        defaultValues: {
+            status: initialState.status,
+            project: initialState.project,
+            labels: initialState.labels,
+        }
+    });
+
     const [state, setState] = React.useState(initialState);
 
     function saveState(event, newValue) {
@@ -57,6 +72,7 @@ function IssueForm(props) {
             name = 'labels';
             value = newValue;
         }
+        setValue(name, value, { shouldValidate: true });
         setState(prevState => ({
             ...prevState,
             [name]: value,
@@ -73,123 +89,147 @@ function IssueForm(props) {
         history.push('/issues/new');
     }
 
+    React.useEffect(() => {
+        register({ name: 'status' }, { required: true });
+        register({ name: 'project' }, {
+            required: true,
+            validate: {
+                key: value => value !== 0
+            }
+        });
+        register({ name: 'labels' }, {
+            required: true,
+            validate: {
+                size: value => value !== undefined ? value.length > 0 : false
+            }
+        });
+    }, [register]);
+
     return (
         <React.Fragment>
             <Paper className={classes.Paper} elevation={0}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Typography variant='h6' color='primary'>
-                            {props.formTitle}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FormControl variant='outlined' fullWidth>
-                            <InputLabel color='primary'>Status</InputLabel>
-                            <Select
-                                id='status'
-                                name='status'
-                                required
-                                label='Status'
-                                value={state.status}
-                                onChange={saveState}
-                                inputProps={{ className: classes.FormInput }}>
-                                <MenuItem key={1} value={1}>Status 1</MenuItem>
-                                <MenuItem key={2} value={2}>Status 2</MenuItem>
-                                <MenuItem key={3} value={3}>Status 3</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <FormControl variant='outlined' fullWidth>
-                            <InputLabel color='primary'>Project</InputLabel>
-                            <Select
-                                id='project'
-                                name='project'
-                                required
-                                disabled={props.issue !== undefined && props.issue !== null}
-                                label='Project'
-                                value={state.project}
-                                onChange={saveState}
-                                inputProps={{ className: classes.FormInput }}>
-                                <MenuItem key={0} value={0}>&nbsp;</MenuItem>
-                                {props.allProjects && props.allProjects.map((project) => (
-                                    <MenuItem key={project.id} value={project.id}>{project.name}</MenuItem>
-                                ))}
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControl variant='outlined' fullWidth>
-                            <TextField
-                                id='title'
-                                name='title'
-                                label='Title'
-                                variant='outlined'
-                                value={state.title}
-                                onChange={saveState}
-                                required
-                                inputProps={{ className: classes.FormInput }} />
-                            <FormHelperText>
-                                Max. 255 characters
+                <form onSubmit={handleSubmit(handleSave)} className={classes.Form}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant='h6' color='primary'>
+                                {props.formTitle}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <FormControl variant='outlined' fullWidth className={classes.FormControl} error={errors.status !== undefined}>
+                                <InputLabel color='primary'>Status</InputLabel>
+                                <Select
+                                    id='status'
+                                    name='status'
+                                    label='Status'
+                                    value={state.status}
+                                    onChange={saveState}
+                                    inputProps={{ className: classes.FormInput }}>
+                                    <MenuItem key={1} value={1}>Status 1</MenuItem>
+                                    <MenuItem key={2} value={2}>Status 2</MenuItem>
+                                    <MenuItem key={3} value={3}>Status 3</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12} md={6}>
+                            <FormControl variant='outlined' fullWidth className={classes.FormControl} error={errors.project !== undefined}>
+                                <InputLabel color='primary'>Project</InputLabel>
+                                <Select
+                                    id='project'
+                                    name='project'
+                                    disabled={props.issue !== undefined && props.issue !== null}
+                                    label='Project'
+                                    value={state.project}
+                                    onChange={saveState}
+                                    inputProps={{ className: classes.FormInput }}>
+                                    <MenuItem key={0} value={0}>&nbsp;</MenuItem>
+                                    {props.allProjects && props.allProjects.map((project) => (
+                                        <MenuItem key={project.id} value={project.id}>{project.name}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl variant='outlined' fullWidth className={classes.FormControl} error={errors.title !== undefined}>
+                                <TextField
+                                    id='title'
+                                    name='title'
+                                    label='Title'
+                                    variant='outlined'
+                                    value={state.title}
+                                    onChange={saveState}
+                                    inputRef={register({ required: true, minLength: 3, maxLength: 500 })}
+                                    error={errors.title !== undefined}
+                                    inputProps={{ className: classes.FormInput }} />
+                                <FormHelperText>
+                                    {errors.title && errors.title.type === "required" && <span>This field is required.</span>}
+                                    {errors.title && errors.title.type === "minLength" && <span>Field value should have at least 3 characters.</span>}
+                                    {errors.title && errors.title.type === "maxLength" && <span>Field value should have at most 500 characters.</span>}
+                                </FormHelperText>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl variant='outlined' fullWidth className={classes.FormControl} error={errors.description !== undefined}>
+                                <TextField
+                                    id='description'
+                                    name='description'
+                                    label='Description'
+                                    variant='outlined'
+                                    multiline
+                                    rows={5}
+                                    value={state.description}
+                                    onChange={saveState}
+                                    inputRef={register({ required: true, minLength: 3, maxLength: 1000 })}
+                                    error={errors.description !== undefined}
+                                    inputProps={{ className: classes.FormInput }} />
+                                <FormHelperText>
+                                    {errors.description && errors.description.type === "required" && <span>This field is required.</span>}
+                                    {errors.description && errors.description.type === "minLength" && <span>Field value should have at least 3 characters.</span>}
+                                    {errors.description && errors.description.type === "maxLength" && <span>Field value should have at most 1000 characters.</span>}
+                                </FormHelperText>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl variant='outlined' fullWidth className={classes.FormControl} error={errors.labels !== undefined}>
+                                <LabelsAutocomplete
+                                    options={props.allLabels || []}
+                                    value={state.labels}
+                                    onChange={saveState}
+                                    error={errors.labels !== undefined} />
+                                <FormHelperText>
+                                    Labels
                             </FormHelperText>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControl variant='outlined' fullWidth>
-                            <TextField
-                                id='description'
-                                name='description'
-                                label='Description'
-                                variant='outlined'
-                                multiline
-                                rows={5}
-                                value={state.description}
-                                onChange={saveState}
-                                inputProps={{ className: classes.FormInput }} />
-                            <FormHelperText>
-                                Max. 1000 characters
-                            </FormHelperText>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormControl variant='outlined' fullWidth>
-                            <LabelsAutocomplete
-                                options={props.allLabels || []}
-                                value={state.labels}
-                                onChange={saveState} />
-                            <FormHelperText>
-                                Labels
-                            </FormHelperText>
-                        </FormControl>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                            variant='contained'
-                            className={classes.FormButton}
-                            size='large'
-                            onClick={props.onCancel}
-                            startIcon={<Cancel />}>
-                            Cancel
-                        </Button>
-                        <Button
-                            variant='contained'
-                            className={classes.FormButton}
-                            size='large'
-                            onClick={handleSave}
-                            startIcon={<Save />}>
-                            Save
-                        </Button>
-                        {id !== '' &&
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
                             <Button
                                 variant='contained'
                                 className={classes.FormButton}
                                 size='large'
-                                onClick={handleAddNew}
+                                onClick={props.onCancel}
+                                startIcon={<Cancel />}>
+                                Cancel
+                        </Button>
+                            <Button
+                                variant='contained'
+                                className={classes.FormButton}
+                                size='large'
+                                type='submit'
                                 startIcon={<Save />}>
-                                Add New
+                                Save
+                        </Button>
+                            {id !== '' &&
+                                <Button
+                                    variant='contained'
+                                    className={classes.FormButton}
+                                    size='large'
+                                    onClick={handleAddNew}
+                                    startIcon={<Save />}>
+                                    Add New
                         </Button>}
+                        </Grid>
                     </Grid>
-                </Grid>
+                </form>
             </Paper>
         </React.Fragment>
     );
